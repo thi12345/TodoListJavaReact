a6039e2980e2dca10e3e2c6d654c1c0d52c5f0e9
thi12345 <thinguyen130304@gmail.com>
2025-05-13 00:27:40 +0700

    first commit setup springboot

---
+++ ems-be/src/main/java/com/java/ems_be/EmsBeApplication.java
@@ -0,0 +1,13 @@
+package com.java.ems_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsBeApplication.class, args);
+	}
+
+}
---
+++ ems-be/src/test/java/com/java/ems_be/EmsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.ems_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
